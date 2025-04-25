49a6b37dce8752902c00190e68f44d337a6a8b66
Ian <168940510+IanSegade@users.noreply.github.com>
2025-04-25 08:04:22 -0300

    Delete TP01_REACT_SEGADE_DAGOSTINO_CABRAL directory

--- TP01_REACT_SEGADE_DAGOSTINO_CABRAL/src/components/header.jsx
+++
@@ -1,8 +0,0 @@
-const Header = () => (
-    <h1 className="text-5xl font-black text-center md:w-2/3 mx-auto">
-      Seguimiento Pacientes <span className="text-indigo-600">Veterinaria</span>
-    </h1>
-  );
-  
-  export default Header;
-  
